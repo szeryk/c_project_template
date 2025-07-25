efe41a35e2676654a771f1994390bca2a1fb8986
Eryk Szyfner <eryk.szyfner@gmail.com>
2025-07-25 19:24:29 +0200

    Create first version of C project template
    The template consists of small C program, one example library and
    CMake configuration to build it all.

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "my_lib.h"
+
+int main() {
+    printf("Hello, World!\n");
+
+    printf("The secret number is: %d\n", get_secret_number());
+    return 0;
+}
+
---
+++ src/my_lib.c
@@ -0,0 +1,5 @@
+#include "my_lib.h"
+
+int get_secret_number() {
+    return 42;
+}
